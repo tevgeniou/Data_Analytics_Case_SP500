f3d67a558e299a8d3fef49d86900aca936465fde
theodoros evgeniou <theodoros.evgeniou@insead.edu>
2014-01-12 13:57:21 +0100

    minor (start_local_webapp <- 0) by default

--- RunStudy.R
+++ RunStudy.R
@@ -42,7 +42,7 @@ use_mean_alpha <- 0
 # 1: start application on LOCAL computer, 0: do not start it
 # SELECT 0 if you are running the application on a server 
 # (DEFAULT is 0). 
-start_local_webapp <- 1
+start_local_webapp <- 0
 
 ################################################
 # Now run everything
